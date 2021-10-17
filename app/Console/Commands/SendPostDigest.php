1edd53226a9e5bacedfe99ea22716d40730b4f5c
Alexey Mashkarin <alexey.mashkarin@ottoruss.ru>
2021-10-17 10:49:06 +0300

    Command and Schedule

---
+++ app/Console/Commands/SendPostDigest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\PostsDigest;
+use App\Models\Post;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+
+class SendPostDigest extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'post_digest:send
+    {days_ago : Количество дней назад от момента запуска команды. Определяет начало периода выборки}
+    {days : Длина периода в днях для выборки статей }
+    ';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Отсылает информацию об опубликованных статьях за указанный период';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $daysAgo = $this->argument('days_ago');
+        $days = $this->argument('days');
+        $startPeriodDate = (new Carbon())->subDays($daysAgo)->startOfDay();
+        $endPeriodTime = (clone $startPeriodDate)->addDays($days)->endOfDay();
+
+        $emails = User::all()->pluck('email');
+
+        $posts = Post::where('created_at', '>=', $startPeriodDate)
+            ->where('created_at', '<=', $endPeriodTime)
+            ->where('published', true)
+            ->orderByDesc('created_at')
+            ->get();
+
+        if ($posts->count() == 0) {
+            $this->warn('Не найдено ни одной статьи за указанный период');
+            return Command::SUCCESS;
+        }
+
+        \Mail::to($emails)->send(new PostsDigest($posts));
+        $this->info('Рассылка отправлена на адреса: ' . $emails->implode(','));
+
+        return Command::SUCCESS;
+    }
+}
